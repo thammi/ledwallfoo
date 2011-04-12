5954f5890e9dbef221c71f981e76b5e12ee78cf1
Jens Abraham (core) <Jens_Abraham@gmx.de>
2011-04-13 00:14:37 +0200

    [dvb-next.py] working example with some utf-8 symbolds

--- dvb-next.py
+++ dvb-next.py
@@ -3,23 +3,29 @@
 import json
 import urllib
 
-#wget --user-agent=foo -o- --header='Connection: close' --header='Accept: */*' -O -
+from fade_text import FadingText
+from ledwall import LedMatrix
 
-dvbhandle = urllib.urlopen('http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=Dresden&hst=Pirnaischer+Platz&vs=0')
+def fetchDVBData():
+    dvbhandle = urllib.urlopen('http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=Dresden&hst=Pirnaischer+Platz&vs=0')
 
-rawjson = dvbhandle.read()
-data = json.loads(rawjson)
+    rawjson = dvbhandle.read()
+    data = json.loads(rawjson)
 
-output = u''
+    output = u''
 
-for line in data:
-    entry = tuple(line)
-    stripped_linenumber = int(entry[0].replace('E', ''))
-    if stripped_linenumber in [1,2,3,4,6,7,8,9,10,11,12,13]:
-        output += u'\U0001F68B %s \U00002794 %s \U00008986 %s\t' % entry # tram
-    elif stripped_linenumber > 60:
-        output += u'\U0001F68C %s \U00002794 %s \U00008986 %s\t' % entry # bus
-    else:
-        output += '? %s \U00002794 %s \U00008986 %s\t' % entry
+    for line in data:
+        entry = tuple(line)
+        stripped_linenumber = int(entry[0].replace('E', ''))
+        if stripped_linenumber in [1,2,3,4,6,7,8,9,10,11,12,13]:
+            output += u'T %s \U00002192 %s \U00002694 %s +++' % entry # tram
+        elif stripped_linenumber > 60:
+            output += u'B %s \U00002192 %s \U00002694 %s +++' % entry # bus
+        else:
+            output += '? %s \U00002192 %s \U00002694 %s +++' % entry
+    return output
 
-print output
+if __name__ == '__main__':
+    matrix = LedMatrix()
+    ft = FadingText(matrix, fetchDVBData()).endless()
+
