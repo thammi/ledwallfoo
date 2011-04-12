2837f0dc9d4c3fbb73c13fce7b14ae40454c0bcc
Jens Abraham (core) <Jens_Abraham@gmx.de>
2011-04-12 23:00:37 +0200

    [dvb-next.py] different symbols for different services

--- dvb-next.py
+++ dvb-next.py
@@ -10,5 +10,16 @@ dvbhandle = urllib.urlopen('http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrt
 rawjson = dvbhandle.read()
 data = json.loads(rawjson)
 
+output = u''
+
 for line in data:
-    print u'\U0001F68B %s \U00002794 %s \U00008986 %s' % tuple(line)
+    entry = tuple(line)
+    stripped_linenumber = int(entry[0].replace('E', ''))
+    if stripped_linenumber in [1,2,3,4,6,7,8,9,10,11,12,13]:
+        output += u'\U0001F68B %s \U00002794 %s \U00008986 %s\t' % entry # tram
+    elif stripped_linenumber > 60:
+        output += u'\U0001F68C %s \U00002794 %s \U00008986 %s\t' % entry # bus
+    else:
+        output += '? %s \U00002794 %s \U00008986 %s\t' % entry
+
+print output
